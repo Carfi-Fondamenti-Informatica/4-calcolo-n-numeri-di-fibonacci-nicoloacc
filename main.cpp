8d3b7371efe04c15bde1b75824889c33132cb36d
nicoloacc <100343394+nicoloacc@users.noreply.github.com>
2022-03-30 21:33:53 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main() {
+    int n=0, risultato=0, a=0, b=1;
+    cout << "inserire i valori di n" << endl;
+    cin >> n;
+    if (n>=2){
+        for (int i = 1; i<=n; i++) {
+            risultato = a+b;
+            b=a;
+            a=risultato;
+            cout << risultato << endl;
+        }
+
+    }
+    else {
+        cout << "errore" << endl;
+       ;
+    }
    return 0;
 }
